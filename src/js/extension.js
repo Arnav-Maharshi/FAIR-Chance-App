94262a035944bc2b4d71f7dc186edc4b483877d4
Arnav-Maharshi <64527961+Arnav-Maharshi@users.noreply.github.com>
2025-08-17 00:42:00 +0530

    Fixing minor buga

--- src/js/extension.js
+++ src/js/extension.js
@@ -272,7 +272,7 @@ async function renderLoop() {
 
 document.getElementById('exportButton').onclick = function() {
     // Use your actual variable names and angle labels
-    myUtils.exportAngle_AccScoreData(angleHistory, accScoreHistory, selectedMode, "extension", isNative);
+    myUtils.exportAngle_AccScoreData(angleHistory, accScoreHistory, timestampHistory, selectedMode, "extension", isNative);
 };
 
 // Reset the app to initial state (trying to optimize responsiveness, avoiding reload)
